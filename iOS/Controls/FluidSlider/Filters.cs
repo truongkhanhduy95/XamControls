da5021ed31fbcbb94dfb4a29d947d910ef1fbf04
cvn_duy_truong <duy.truong@contemi.com.vn>
2018-07-10 12:14:48 +0700

    init FluidSlider iOS

---
+++ iOS/Controls/FluidSlider/Filters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CoreImage;
+using UIKit;
+
+namespace XamControls.iOS.Controls
+{
+    public class MetaballFilter : CIFilter
+    {
+        private CIImage inputImage;
+
+        private float blurRadius = 12;
+        private float threshold = 0.75f;
+        private UIColor backgroundColor;
+        private float antialiasingRadius = (float)(UIScreen.MainScreen.Scale / 2);
+
+        public override CIImage OutputImage
+        {
+            //var inputImage = inputImage != null ? inputImage : null;
+            return inputImage;
+
+        }
+    }
+}

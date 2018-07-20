5ae813003a47c2d8fc3d639a32d52604ec8f7480
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-07-20 15:43:31 +0700

    Init sample

--- iOS/ViewControllers/AboutViewController.cs
+++ iOS/ViewControllers/AboutViewController.cs
@@ -17,11 +17,8 @@ namespace XamControls.iOS
 
             Title = ViewModel.Title;
 
-            AppNameLabel.Text = "XamControls";
-            VersionLabel.Text = "1.0";
-            AboutTextView.Text = "This app is written in C# and native APIs using the Xamarin Platform. It shares code with its iOS, Android, & Windows versions.";
+           
         }
 
-        partial void ReadMoreButton_TouchUpInside(UIButton sender) => ViewModel.OpenWebCommand.Execute(null);
     }
 }

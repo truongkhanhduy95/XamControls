e2c15d705ad900b0a3f2e348999b9df0dcfb5e39
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-07-20 15:29:12 +0700

    Init Page View

---
+++ iOS/Controls/PaperOnboarding/PageView/Item/PageViewItem.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using UIKit;
+
+namespace XamControls.iOS.Controls
+{
+    public class PageViewItem : UIView
+    {
+        private float circleRadius;
+        private float selectedCircleRadius;
+        private float lineWidth;
+        private UIColor itemColor;
+
+        private bool select;
+
+        private UIView centerView;
+        public UIImageView ImageView;
+        private CAShapeLayer circleLayer;
+        private int tickIndex;
+
+        public PageViewItem(float radius,
+                           UIColor color,
+                           float selectedRadius,
+                            bool isSelect = false,
+                           float lineWidth = 3
+                           )
+        {
+            itemColor = color;
+            this.lineWidth = lineWidth;
+            circleRadius = radius;
+            selectedCircleRadius = selectedRadius;
+            select = isSelect;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            centerView = CreateBorderView();
+            ImageView = CreateImageView();
+        }
+
+        public void AnimationSelected(bool selected, double duration, bool fillColor)
+        {
+            var toAlpha = select ? 1 : 0;
+            ImageAlphaAnimation(toAlpha, duration);
+
+            var currentRad = selected ? selectedCircleRadius : circleRadius;
+            var scaleAnim = CircleScaleAnimation(currentRad - lineWidth / 2, duration);
+            var toColor = fillColor ? itemColor : UIColor.Clear;
+            var colorAnim = CircleBackgroundAnimation(toColor, duration);
+
+            circleLayer?.AddAnimation(scaleAnim, null);
+            circleLayer?.AddAnimation(colorAnim, null);
+        }
+
+        private UIView CreateBorderView()
+        {
+            var view = new UIView(CGRect.Empty);
+            view.BackgroundColor = UIColor.Blue;
+            view.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            this.AddSubview(view);
+
+            // Create circle layer
+            var currentRadius = select ? selectedCircleRadius : circleRadius;
+            var clLayer = CreateCircleShapeLayer(currentRadius, lineWidth);
+            view.Layer.AddSublayer(clLayer);
+            this.circleLayer = clLayer;
+
+            // Add constraints
+            (this, view).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.CenterX);
+            (this, view).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.CenterY);
+            view.ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Height);
+            view.ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Width);
+
+            return view;
+        }
+
+        private UIImageView CreateImageView()
+        {
+            var imgView = new UIImageView(CGRect.Empty);
+            imgView.ContentMode = UIViewContentMode.ScaleAspectFit;
+            imgView.TranslatesAutoresizingMaskIntoConstraints = false;
+            imgView.Alpha = select ? 1 : 0;
+
+            this.AddSubview(imgView);
+
+            // Add constraints
+            (this, imgView).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Left);
+            (this, imgView).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Right);
+            (this, imgView).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Top);
+            (this, imgView).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Bottom);
+
+            return imgView;
+        }
+
+        private CAShapeLayer CreateCircleShapeLayer(float radius, float lineW)
+        {
+            var path = UIBezierPath.FromArc(CGPoint.Empty,
+                                           radius - lineW / 2,
+                                           0,
+                                            (nfloat)(2 * Math.PI), true);
+            var layer = new CAShapeLayer();
+            layer.Path = path.CGPath;
+            layer.LineWidth = lineW;
+            layer.StrokeColor = itemColor.CGColor;
+            layer.FillColor = UIColor.Clear.CGColor;
+
+            return layer;
+        }
+
+        private CABasicAnimation CircleScaleAnimation(float toRadius, double duration)
+        {
+            var path = UIBezierPath.FromArc(CGPoint.Empty,
+                                           toRadius,
+                                           0,
+                                            (nfloat)(2 * Math.PI), true);
+            var animation = CABasicAnimation.FromKeyPath("path");
+            animation.Duration = duration;
+            animation.SetTo(path.CGPath);
+            animation.RemovedOnCompletion = false;
+            animation.FillMode = CAFillMode.Forwards;
+            return animation;
+        }
+
+        private CABasicAnimation CircleBackgroundAnimation(UIColor toColor, double duration)
+        {
+           
+            var animation = CABasicAnimation.FromKeyPath("fillColor");
+            animation.Duration = duration;
+            animation.SetTo(toColor.CGColor);
+            animation.RemovedOnCompletion = false;
+            animation.FillMode = CAFillMode.Forwards;
+            animation.TimingFunction = CAMediaTimingFunction.FromName(CAMediaTimingFunction.EaseInEaseOut);
+            return animation;
+        }
+
+        private void ImageAlphaAnimation(float toValue, double duration)
+        {
+            UIView.Animate(duration, () =>
+             {
+                ImageView.Alpha = toValue;
+             });
+        }
+    }
+}
---
+++ iOS/Controls/PaperOnboarding/PageView/PageContainerView/PageContainer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace XamControls.iOS.Controls
+{
+    public class PageContainer : UIView
+    {
+        public List<PageViewItem> Items;
+        private float space; //space between items
+        private int currentIndex = 0;
+
+        private float itemRadius;
+        private float selectedItemRadius;
+        private int itemsCount;
+        private string animationKey = "animationKey";
+
+        public PageContainer(float radius,
+                            float selectedRadius,
+                            float space,
+                            int itemsCount,
+                            UIColor itemColor)
+        {
+            this.itemsCount = itemsCount;
+            this.space = space;
+            itemRadius = radius;
+            selectedItemRadius = selectedRadius;
+
+            Items = CreateItems(itemsCount, radius, selectedRadius, itemColor);
+        }
+
+        public void CurrentIndex(int index, double duration, bool animated)
+        {
+            var items = this.Items;
+            if (index == currentIndex) return; //TODO: CHECK HERE
+
+            AnimationItem(items[index], true, duration);
+
+            var fillColor = index > currentIndex ? true : false;
+            AnimationItem(items[index], false, duration, fillColor);
+
+            currentIndex = index;
+        }
+
+        protected void AnimationItem(PageViewItem item, bool selected, double duration, bool fillColor = false)
+        {
+            var toValue = selected ? selectedItemRadius : itemRadius * 2;
+            foreach(var constraint in item.Constraints)
+            {
+                if (constraint.GetIdentifier() == animationKey)
+                    constraint.Constant = toValue;
+            }
+
+            UIView.Animate(duration, 0, UIViewAnimationOptions.CurveEaseOut, () =>
+            {
+                this.LayoutIfNeeded();
+            }, () => { });
+
+            item.AnimationSelected(selected, duration, fillColor);
+        }
+
+        private List<PageViewItem> CreateItems(int count, float radius, float selectedRadius, UIColor itemColor)
+        {
+            var _items = new List<PageViewItem>();
+
+            // Create first item
+            var tag = 1;
+            var item = CreateItem(radius, selectedRadius, itemColor, true);//itemColor(tag - 1));
+            item.Tag = tag;
+            AddConstraintsToView(item, selectedRadius);
+            _items.Add(item);
+
+            for (var i = 1; i < count; i++)
+            {
+                tag += 1;
+                var nextItem = CreateItem(radius, selectedRadius, itemColor, true);//itemColor(tag - 1));
+                AddConstraintsToView(nextItem, item, radius);
+                Items.Add(nextItem);
+                item = nextItem;
+                item.Tag = tag;
+            }
+            return _items;
+        }
+
+        private PageViewItem CreateItem(float radius, float selectedRadius, UIColor itemColor, bool isSelect = false)
+        {
+            var item = new PageViewItem(radius, itemColor, selectedRadius, isSelect);
+            item.TranslatesAutoresizingMaskIntoConstraints = false;
+            item.BackgroundColor = UIColor.Clear;
+
+            this.AddSubview(item);
+
+            return item;
+        }
+
+        private void AddConstraintsToView(UIView item, float radius)
+        {
+            (this, item).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.Left);
+            (this, item).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.CenterY);
+
+            var attrs = new NSLayoutAttribute[] { NSLayoutAttribute.Width, NSLayoutAttribute.Height };
+
+            foreach(var att in attrs)
+                item.ConstraintOps((obj) =>
+                {
+                    obj.Attribute = att;
+                    obj.Constant = (System.nfloat)(radius * 2.0);
+                    obj.Identifer = animationKey;
+                });
+        }
+
+        private void AddConstraintsToView(UIView item, UIView leftView, float radius)
+        {
+            (this, item).ConstraintOps((obj) => obj.Attribute = NSLayoutAttribute.CenterY);
+
+            (this, item, leftView).ConstraintOps((obj) => 
+            {
+                obj.Attribute = NSLayoutAttribute.Leading;
+                obj.SecondAttribute = NSLayoutAttribute.Trailing;
+                obj.Constant = space;
+            });
+
+
+            var attrs = new NSLayoutAttribute[] { NSLayoutAttribute.Width, NSLayoutAttribute.Height };
+
+            foreach (var att in attrs)
+                item.ConstraintOps((obj) =>
+                {
+                    obj.Attribute = att;
+                    obj.Constant = (System.nfloat)(radius * 2.0);
+                    obj.Identifer = animationKey;
+                });
+        }
+    }
+}

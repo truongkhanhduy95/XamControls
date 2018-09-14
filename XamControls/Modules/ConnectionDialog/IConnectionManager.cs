bd597650b89453782234d231b3625409ebe97de8
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-09-14 10:40:59 +0700

    Init Connectivity shared

---
+++ XamControls/Modules/ConnectionDialog/IConnectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace XamControls.Modules
+{
+    public interface IConnectionManager
+    {
+        bool IsWifiConnected();
+
+        bool Is3GConnected();
+
+        void NavigateToConnectionSettings();
+    }
+}

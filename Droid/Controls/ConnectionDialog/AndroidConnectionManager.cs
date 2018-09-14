4726ddd37609b2438829c95cda705ef68933e199
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-09-14 10:41:25 +0700

    Implement Connectivity android

---
+++ Droid/Controls/ConnectionDialog/AndroidConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using XamControls.Modules;
+
+namespace XamControls.Droid.Controls
+{
+    public class AndroidConnectionManager : IConnectionManager
+    {
+        public AndroidConnectionManager()
+        {
+        }
+
+        public bool Is3GConnected()
+        {
+            return false;
+        }
+
+        public bool IsWifiConnected()
+        {
+            return false;
+        }
+
+        public void NavigateToConnectionSettings()
+        {
+
+        }
+    }
+}

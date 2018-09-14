51ebadc5cc32181d1e5e2dd201b00769665a8548
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-09-14 10:41:39 +0700

    Implement Connectivity iOS

---
+++ iOS/Controls/ConnectionDialog/IOSConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using XamControls.Modules;
+
+namespace XamControls.iOS.Controls
+{
+    public class IOSConnectionManager : IConnectionManager
+    {
+        public IOSConnectionManager()
+        {
+        }
+
+        public bool Is3GConnected()
+        {
+            return false;
+        }
+
+        public bool IsWifiConnected()
+        {
+            return false;
+        }
+
+        public void NavigateToConnectionSettings()
+        {
+        }
+    }
+}
